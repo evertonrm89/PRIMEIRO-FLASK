996abe7d6f31428fc24589fde6dd535270388adb
evertonrm89 <evertonrm89@gmail.com>
2024-11-15 20:35:25 -0300

    15/11 loja

--- app.py
+++ app.py
@@ -16,5 +16,14 @@ def saudacao():
 def curso():
     return render_template("cursos.html")
 
+@app.route('/cursos/<nome>')
+def curso_com_nome(nome):
+    if nome == 'devweb':
+        return render_template("curso_devweb.html")
+    elif nome == 'poo':
+        return render_template("curso_poo.html")
+    else:
+        return "Curso não existe"
+
 if __name__ == '__main__':
     app.run(debug=True)
